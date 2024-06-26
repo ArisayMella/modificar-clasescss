bfd581f53ba81df62f12c3cbf86a952126479057
ArisayMella <163840802+ArisayMella@users.noreply.github.com>
2024-06-26 11:41:00 +0200

    up

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,29 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+//const boxElement = document.getElementById('box');
+
+//const buttonElement = document.getElementById('button');
+
+//boxElement.classList.replace('box');
+
+/*
+buttonElement.addEventListener('click', () => {
+  if (boxElement.classList.contains('box--green')) {
+    boxElement.classList.remove('box--green');
+  } else {
+    boxElement.classList.add('box--green');
+  }
+});*/
+
+const formElement = document.getElementById('form');
+
+const checkForm = event => {
+  event.preventDefaukt();
+  console.dir(event.target);
+};
+
+formElement.addEventListener('submit', checkForm);
+formElement.addEventListener('input', event => {
+  console.log(event.target.value);
+});
